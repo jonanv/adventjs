a40d4f504642af1ff684775d355ed2d5fc3e3714
Johanny Vargas González <jonan-vargas23@hotmail.com>
2022-12-11 22:55:56 -0500

    :art: Mejora de estructura reto 11

--- challenge11/index.js
+++ challenge11/index.js
@@ -1,72 +1,72 @@
 // score: 260
 // function getCompleted(part, total) {
 //     let secT = total.split(":").reverse()
-//         .reduce((a, b, i) => a + parseInt(b * (Math.pow(60, i))), 0)
+//         .reduce((a, b, i) => a + parseInt(b * (Math.pow(60, i))), 0);
 //     let secP = part.split(":").reverse()
-//         .reduce((a, b, i) => a + parseInt(b * (Math.pow(60, i))), 0)
+//         .reduce((a, b, i) => a + parseInt(b * (Math.pow(60, i))), 0);
 
-//     let numeroGrande = secT / secP
-//     let cont = 1
+//     let numeroGrande = secT / secP;
+//     let cont = 1;
 
 //     while (numeroGrande % 1 !== 0) {
-//         cont++
-//         numeroGrande = numeroGrande * cont
+//         cont++;
+//         numeroGrande = numeroGrande * cont;
 //     }
-//     return `${cont}/${(secT / secP) * cont}`
+//     return `${cont}/${(secT / secP) * cont}`;
 // }
 
 // score: 220
 // function getCompleted(part, total) {
-//     let secT = total.split(":")
-//     secT = (parseInt(secT[0]) * 3600) + (parseInt(secT[1] * 60)) + (parseInt(secT[2]))
-//     let secP = part.split(":")
-//     secP = (parseInt(secP[0]) * 3600) + (parseInt(secP[1] * 60)) + (parseInt(secP[2]))
+//     let secT = total.split(":");
+//     secT = (parseInt(secT[0]) * 3600) + (parseInt(secT[1] * 60)) + (parseInt(secT[2]));
+//     let secP = part.split(":");
+//     secP = (parseInt(secP[0]) * 3600) + (parseInt(secP[1] * 60)) + (parseInt(secP[2]));
 
-//     let numeroGrande = secT / secP
-//     let cont = 1
+//     let numeroGrande = secT / secP;
+//     let cont = 1;
 
 //     while (numeroGrande % 1 !== 0) {
-//         cont++
-//         numeroGrande = numeroGrande * cont
+//         cont++;
+//         numeroGrande = numeroGrande * cont;
 //     }
-//     return `${cont}/${(secT / secP) * cont}`
+//     return `${cont}/${(secT / secP) * cont}`;
 // }
 
 // score: 360
 // function getCompleted(part, total) {
-//     const arrPart = part.split(':')
-//     const arrTotal = total.split(':')
-//     const partSeconds = arrPart[0] * 3600 + arrPart[1] * 60 + arrPart[2] * 1
-//     const totalSeconds = arrTotal[0] * 3600 + arrTotal[1] * 60 + arrTotal[2] * 1
-//     let gcd = partSeconds
-//     let tmp = totalSeconds
+//     const arrPart = part.split(':');
+//     const arrTotal = total.split(':');
+//     const partSeconds = arrPart[0] * 3600 + arrPart[1] * 60 + arrPart[2] * 1;
+//     const totalSeconds = arrTotal[0] * 3600 + arrTotal[1] * 60 + arrTotal[2] * 1;
+//     let gcd = partSeconds;
+//     let tmp = totalSeconds;
 //     while (tmp) {
-//         const t = tmp
-//         tmp = gcd % tmp
-//         gcd = t
+//         const t = tmp;
+//         tmp = gcd % tmp;
+//         gcd = t;
 //     }
-//     return `${partSeconds / gcd}/${totalSeconds / gcd}`
+//     return `${partSeconds / gcd}/${totalSeconds / gcd}`;
 // }
 
 // score: 260 - Mia
 function getCompleted(part, total) {
-    let time = [3600, 60, 1]
+    let time = [3600, 60, 1];
     let totalTime = total
         .split(':')
         .map((value, index) => value * time[index])
-        .reduce((a, c) => a + c);
+        .reduce((a, c) => a + c, 0);
     let partTime = part
         .split(':')
         .map((value, index) => value * time[index])
-        .reduce((a, c) => a + c);
+        .reduce((a, c) => a + c, 0);
 
     let fraction = totalTime / partTime;
-    let cont = 1
+    let cont = 1;
     while (fraction % 1 !== 0) {
-        cont++
-        fraction = fraction * cont
+        cont++;
+        fraction = fraction * cont;
     }
-    return `${cont}/${(totalTime / partTime) * cont}`
+    return `${cont}/${(totalTime / partTime) * cont}`;
 }
 
 module.exports = getCompleted;
