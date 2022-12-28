a93436d9525256488cc39f29d2c97cd9949feb89
Johanny Vargas González <jonan-vargas23@hotmail.com>
2022-12-27 22:00:20 -0500

    :hammer: Nueva solución reto 24

--- challenge24/index.js
+++ challenge24/index.js
@@ -135,6 +135,29 @@ function canExit(maze) {
     return result;
 }
 
+// score: 900
+function canExit(maze) {
+    function replace(str) {
+        return str
+            .replace(/[S][\sE]/g, "SS")
+            .replace(/[\sE][S]/g, "SS")
+            .split("");
+    }
+
+    let x = [];
+    new Array(maze[0].length * maze.length).fill(0).forEach(() => {
+        let check = [...maze];
+        maze.map((horizontal, i) => {
+            maze[i] = replace(horizontal.join(""));
+            let v = maze[0].map((_, i) => replace(maze.map((x) => x[i]).join("")));
+            maze[i] = v.map((x) => x[i]);
+        });
+        check.flat().join("") == maze.flat().join("") && x.push(check);
+    });
+
+    return !x.flat(2).includes("E");
+}
+
 // score:
 function canExit(maze) {
     let length = maze.length - 1;
