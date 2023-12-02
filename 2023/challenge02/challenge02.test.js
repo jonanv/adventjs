688831189f621669a15e5e875cea769899b80821
jonanv <jonan-vargas23@hotmail.com>
2023-12-02 12:35:31 -0500

    :white_check_mark: (tests): Add new pass tests

--- 2023/challenge02/challenge02.test.js
+++ 2023/challenge02/challenge02.test.js
@@ -7,3 +7,32 @@ test('Test #1 - Retorna un arreglo', () => {
         )
     ).toStrictEqual(true);
 });
+
+test("Test #2 - manufacture(['tren', 'oso', 'pelota'], 'tronesa')", () => {
+    expect(
+        manufacture(['tren', 'oso', 'pelota'], 'tronesa')
+    ).toStrictEqual(
+        [
+        "tren",
+        "oso"
+        ]
+    );
+});
+
+test("Test #3 - manufacture(['coche', 'muñeca', 'balon'], 'ocmuñalb')", () => {
+    expect(
+        manufacture(['coche', 'muñeca', 'balon'], 'ocmuñalb')
+    ).toStrictEqual([]);
+});
+
+test("Test #4 - manufacture(['patineta', 'robot', 'libro'], 'nopor')", () => {
+    expect(
+        manufacture(['patineta', 'robot', 'libro'], 'nopor')
+    ).toStrictEqual([]);
+});
+
+test("Test #5 - manufacture([], 'letras')", () => {
+    expect(
+        manufacture([], 'letras')
+    ).toStrictEqual([]);
+});
