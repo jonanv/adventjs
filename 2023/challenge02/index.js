db2bcc43a7945472f4df8636c75c59c18ddcda71
jonanv <jonan-vargas23@hotmail.com>
2023-12-04 15:22:08 -0500

    :art: (challenge 02 2023): Add new solution with filter and every

--- 2023/challenge02/index.js
+++ 2023/challenge02/index.js
@@ -1,16 +1,26 @@
 // score: 250
-function manufacture(gifts, materials) {
-    let giftsManufacure = [];
+// function manufacture(gifts, materials) {
+//     let giftsManufacure = [];
 
-    gifts.map((gift) => {
-        let made = true;
-        let arrayGift = gift.split('');
-        arrayGift.map((material) => {
-            if (!materials.includes(material)) return made = false;
-        });
-        if (made) giftsManufacure.push(gift);
-    });
-    return giftsManufacure;
+//     gifts.map((gift) => {
+//         let made = true;
+//         let arrayGift = gift.split('');
+//         arrayGift.map((material) => {
+//             if (!materials.includes(material)) return made = false;
+//         });
+//         if (made) giftsManufacure.push(gift);
+//     });
+//     return giftsManufacure;
+// }
+
+// score: 280
+// function manufacture(gifts, materials) {
+//     const regexp = new RegExp(`[^${ materials }]+`);
+//     return gifts.filter(gift => !regexp.test(gift));
+// }
+
+function manufacture(gifts, materials) {
+    return gifts.filter(g => g.split('').every(m => materials.includes(m)));
 }
 
 module.exports = manufacture;
