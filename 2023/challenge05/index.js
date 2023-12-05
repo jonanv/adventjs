7288bac8e9cd52b03506aa71c4be872601860041
jonanv <jonan-vargas23@hotmail.com>
2023-12-05 16:33:48 -0500

    :sparkles: (challenge05): Add challenge 05 adventjs 2023

---
+++ 2023/challenge05/index.js
@@ -0,0 +1,26 @@
+function cyberReindeer(road, time) {
+    // Code here
+    return [];
+}
+
+module.exports = cyberReindeer;
+
+const road = 'S..|...|..';
+const time = 10; // unidades de tiempo
+const result = cyberReindeer(road, time);
+console.log(result);
+
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
