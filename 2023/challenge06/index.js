198da6c80b5ddb5b59675fdabac976b20cadf8d1
jonanv <jonan-vargas23@hotmail.com>
2023-12-06 17:51:09 -0500

    :art: (challenge 06): Add new solution challenge 06

--- 2023/challenge06/index.js
+++ 2023/challenge06/index.js
@@ -34,6 +34,19 @@ function maxDistance(movements) {
 //     return Math.max(Math.abs(r - l) + n, Math.abs(l - r) + n);
 // }
 
+// score: 390
+// function maxDistance(movements) {
+//     let distance = 0;
+//     let right = movements.match(/>/g)?.length ?? 0;
+//     let left = movements.match(/</g)?.length ?? 0;
+
+//     distance += right;
+//     distance -= left;
+
+//     let extra = movements.length - (right + left);
+//     return Math.abs(distance) + extra;
+// }
+
 module.exports = maxDistance;
 
 const movements = '>>*<'
