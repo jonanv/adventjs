e46891f6351d45565e6ad4426acdc39b37fa59ce
jonanv <jonan-vargas23@hotmail.com>
2023-12-12 14:48:26 -0500

    :art: (challenge 05): Add new solution

--- 2023/challenge05/index.js
+++ 2023/challenge05/index.js
@@ -4,15 +4,33 @@ function cyberReindeer(road, time) {
   const _road = ("." + road.slice(1)).replace(/\|/g, "*");
   let a = 0;
 
-  for(let i = 1; i < time; i++) {
-    if(i == 5) road = road.replace(/\|/g, "*");
+  for (let i = 1; i < time; i++) {
+    if (i == 5) road = road.replace(/\|/g, "*");
     const newRoad = road.replace(/S[\.\*]/, `${_road[a]}S`);
-    if(newRoad != road) a++;
+    if (newRoad != road) a++;
     road = newRoad;
     moves.push(road);
   }
 
   return moves;
+}
+
+// score: 170
+function cyberReindeer(road, time) {
+  const BARRIER = '|'
+  //Opening time
+  const O = 5
+  //Barrier position
+  const B = road.indexOf(BARRIER)
+  road = road.replace('S', '.')
+  return Array.from({ length: time }, (_, t) => {
+    const path = [...road]
+    //Santa's position
+    const sp = Math.min(t, B - 1 + Math.max(0, t - O + 1))
+    path[sp] = 'S'
+    const newRoad = path.join('')
+    return t < O ? newRoad : newRoad.replaceAll(BARRIER, '*')
+  })
 }
 
 module.exports = cyberReindeer;
