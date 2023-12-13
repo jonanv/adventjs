669ce3309287ad2595abe16990f3a588aa3059ec
jonanv <jonan-vargas23@hotmail.com>
2023-12-13 09:27:18 -0500

    :sparkles: (challenge 12): Add new solution for challenge 12

--- 2023/challenge12/index.js
+++ 2023/challenge12/index.js
@@ -35,6 +35,33 @@ function checkIsValidCopy(original, copy) {
 //     return res
 // }
 
+// score: 290
+// function checkIsValidCopy(original, copy) {
+//     // generar la regex
+//     let regex = "^";
+//     const orden = "#+:. ";
+//     for (const char of original) {
+//         let pos = "";
+//         pos += (char);
+//         pos += (char.toLowerCase());
+
+//         let ind = orden.indexOf(char);
+//         // convertir -1 a 0 sin modificar el resto
+//         ind = ind + 1;
+//         ind = ind - !!ind;
+
+//         const tmp = orden.slice(ind);
+//         pos += tmp;
+
+//         regex += `[${pos}]`;
+//     }
+//     regex += "$";
+
+//     // comprobar la regex
+//     const reg = new RegExp(regex);
+//     return reg.test(copy);
+// }
+
 module.exports = checkIsValidCopy;
 
 console.log(checkIsValidCopy('Santa Claus is coming', 'sa#ta cl#us is comin#'));  // true
