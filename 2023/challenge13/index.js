14bab52c8661932dc0d6322e9a319d6f6ab1e131
jonanv <jonan-vargas23@hotmail.com>
2023-12-14 14:18:04 -0500

    :sparkles: (challenge 13): Add another solution for challenge 13

--- 2023/challenge13/index.js
+++ 2023/challenge13/index.js
@@ -1,9 +1,9 @@
 // score: 290
 function calculateTime(deliveries) {
-    let HH = 60*60;
+    let HH = 60 * 60;
     let MM = 60;
     let ts = -7 * HH;
-    
+
     for (const delivery of deliveries) {
         let [h, m, s] = delivery.split(':').map(Number);
         ts += h * HH + m * MM + s;
@@ -18,11 +18,34 @@ function calculateTime(deliveries) {
     const ss = ts.toString();
 
     let response = sing
-        response += (hh).padStart(2, '0') + ':';
-        response += (mm).padStart(2, '0') + ':';
-        response += (ss).padStart(2, '0');
+    response += (hh).padStart(2, '0') + ':';
+    response += (mm).padStart(2, '0') + ':';
+    response += (ss).padStart(2, '0');
     return response;
 }
+
+// socre: 290
+// function calculateTime(deliveries) {
+//     const signo = { true: '-', false: '' }
+//     let time = 7 * 3600
+
+//     for (const delivery of deliveries) {
+//         const [hours, minutes, seconds] = delivery.split(':')
+//         time += - hours * 3600 - minutes * 60 - seconds
+//     }
+//     const bool = time > 0
+//     time = Math.abs(time)
+
+//     const seconds = time % 60
+//     const minutes = Math.trunc(time / 60) % 60
+//     const hours = (Math.trunc(time / 60) - minutes) / 60
+
+//     const formated_time = `${hours}`.padStart(2, '0') + ':'
+//         + `${minutes}`.padStart(2, '0') + ':'
+//         + `${seconds}`.padStart(2, '0')
+
+//     return signo[bool] + formated_time
+// }
 
 module.exports = calculateTime;
 
