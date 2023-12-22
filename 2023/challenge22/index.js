76e1498d1076611e826f8d76c99b60ea1f13f1e7
jonanv <jonan-vargas23@hotmail.com>
2023-12-22 15:04:01 -0500

    :art: (challenge 22): Add another solution for challenge 22

--- 2023/challenge22/index.js
+++ 2023/challenge22/index.js
@@ -1,25 +1,40 @@
 // score: 150
+// function compile(code) {
+//     let pointer = 0;
+//     let counter = 0;
+//     let indexOfReturn = -1;
+
+//     let commands = {
+//         '+': () => counter++,
+//         '-': () => counter--,
+//         '*': () => (counter *= 2),
+//         '%': () => (indexOfReturn = pointer),
+//         '<': () =>
+//             indexOfReturn >= 0
+//                 ? ((pointer = indexOfReturn), (indexOfReturn = -1))
+//                 : (pointer = pointer),
+//         '¿': () =>
+//             (pointer = counter > 0 
+//                 ? pointer 
+//                 : code.slice(pointer).indexOf('?') + pointer),
+//     };
+
+//     while (code[pointer]) commands[code[pointer++]]?.();
+//     return counter;
+// }
+
+// score: 170
 function compile(code) {
-    let pointer = 0;
     let counter = 0;
-    let indexOfReturn = -1;
-
-    let commands = {
-        '+': () => counter++,
-        '-': () => counter--,
-        '*': () => (counter *= 2),
-        '%': () => (indexOfReturn = pointer),
-        '<': () =>
-            indexOfReturn >= 0
-                ? ((pointer = indexOfReturn), (indexOfReturn = -1))
-                : (pointer = pointer),
-        '¿': () =>
-            (pointer = counter > 0 
-                ? pointer 
-                : code.slice(pointer).indexOf('?') + pointer),
-    };
-
-    while (code[pointer]) commands[code[pointer++]]?.();
+    let ret = null;
+    for (let i = 0; i < code.length; i++) {
+        counter += +(code[i] == '+');
+        counter -= +(code[i] == '-');
+        counter = [counter, counter * 2][+(code[i] == '*')];
+        ret = [ret, counter][+(code[i] == '%')];
+        counter += [0, 0, counter - ret][+(ret != null) + +(code[i] == '<')];
+        i = [i, i, code.indexOf('?')][+(counter <= 0) + +(code[i] == '¿')];
+    }
     return counter;
 }
 
