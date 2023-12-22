6fe1cd212a05592eac6dd4cdeb0a390132b80fc6
jonanv <jonan-vargas23@hotmail.com>
2023-12-22 14:51:33 -0500

    :sparkles: (challenge 22): Add solution for challenge 22

--- 2023/challenge22/index.js
+++ 2023/challenge22/index.js
@@ -1,5 +1,41 @@
+// score: 150
 function compile(code) {
-    return 0
+    let pointer = 0;
+    let counter = 0;
+    let indexOfReturn = -1;
+
+    let commands = {
+        '+': () => counter++,
+        '-': () => counter--,
+        '*': () => (counter *= 2),
+        '%': () => (indexOfReturn = pointer),
+        '<': () =>
+            indexOfReturn >= 0
+                ? ((pointer = indexOfReturn), (indexOfReturn = -1))
+                : (pointer = pointer),
+        '¿': () =>
+            (pointer = counter > 0 
+                ? pointer 
+                : code.slice(pointer).indexOf('?') + pointer),
+    };
+
+    while (code[pointer]) commands[code[pointer++]]?.();
+    return counter;
 }
 
 module.exports = compile;
+
+// console.log(compile('++*-')); // 3
+// (1 + 1) * 2 - 1 = 3
+
+console.log(compile('++%++<')) // 6
+// // 1 + 1 + 1 + 1 + 1 + 1 = 6
+
+// console.log(compile('++<--')) // 0
+// // 1 + 1 - 1 - 1 = 0
+
+// console.log(compile('++¿+?')) // 3
+// // 1 + 1 + 1 = 3
+
+// console.log(compile('--¿+++?')) // -2
+// // - 1 - 1 = -2
