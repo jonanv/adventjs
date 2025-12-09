1ba33ac7ed327a6899f68357df6b27be089c9a14
jonanv <jonan-vargas23@hotmail.com>
2025-12-09 08:17:37 -0500

    :art: (challenge05): Add another solution to challenge 05

--- 2025/challenge05/index.js
+++ 2025/challenge05/index.js
@@ -6,7 +6,6 @@
 // Score: 8
 function timeUntilTakeOff(fromTime, takeOffTime) {
     // All your code here
-    
     function getTimeStamp(value) {
         let time = value
             .replaceAll('*', '/')
@@ -14,7 +13,7 @@ function timeUntilTakeOff(fromTime, takeOffTime) {
             .replaceAll('@', ' ')
             .split(' ');
         let ts = time[0].concat(' ', time[1]);
-        
+
         const fechaObj = new Date(ts);
         const timestamp = fechaObj.getTime();
         return Math.floor(timestamp / 1000);
@@ -22,5 +21,31 @@ function timeUntilTakeOff(fromTime, takeOffTime) {
 
     return getTimeStamp(takeOffTime) - getTimeStamp(fromTime);
 }
+
+// function timeUntilTakeOff(fromTime, takeOffTime) {
+//     const from = parseElfDate(fromTime);
+//     const takeOff = parseElfDate(takeOffTime);
+
+//     const diff = Math.floor((takeOff - from) / 1000);
+//     return diff;
+// }
+
+// function parseElfDate(elfTime) {
+//     // Formato: YYYY*MM*DD@HH|mm|ss NP
+//     // Eliminar ' NP'
+//     elfTime = elfTime.slice(0, -3);
+
+//     // Separar fecha y hora
+//     const [date, time] = elfTime.split('@');
+
+//     // Fecha: YYYY*MM*DD
+//     const [Y, M, D] = date.split('*').map(Number);
+
+//     // Hora: HH|mm|ss
+//     const [h, m, s] = time.split('|').map(Number);
+
+//     // Se trata como UTC (sin TZs)
+//     return Date.UTC(Y, M - 1, D, h, m, s);
+// }
 
 module.exports = timeUntilTakeOff;
